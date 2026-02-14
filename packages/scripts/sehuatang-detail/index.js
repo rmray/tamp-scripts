05a19e0e0a55a83bf9aa6787ee25e5c7d9fcde70
Ray <1497161283@qq.com>
2026-02-14 15:30:19 +0800

    feat(sehuatang-detail): 搜索番号/女优功能

--- packages/scripts/sehuatang-detail/index.js
+++ packages/scripts/sehuatang-detail/index.js
@@ -15,7 +15,7 @@ const id = getId() // 获取帖子ID
 const paginationEl = document.getElementById('pgt') // 头部分页行
 
 // 书签功能
-let bookmarkBtn = null // 书签按钮
+let bookmarkBtns = [] // 书签按钮
 
 // Ban/Fav 功能
 let idolNames = [] // 帖子中出现的女优名字列表
@@ -24,6 +24,17 @@ let banBtn = null // Ban 按钮
 let favBtn = null // Fav 按钮
 let unBanBtn = null // Unban 按钮
 let unFavBtn = null // Unfav 按钮
+
+// 搜索番号功能
+const SEHUATANG_SEARCH_BASE_URL =
+  '/search.php?mod=forum&searchid=0&searchmd5=718fd9a1791fbce8968a682435a51b33&orderby=lastpost&ascdesc=desc&searchsubmit=yes&kw='
+const JAVBUS_SEARCH_BASE_URL = 'https://www.busjav.bond/'
+let searchBaseUrls = [SEHUATANG_SEARCH_BASE_URL, JAVBUS_SEARCH_BASE_URL] // 目标搜索引擎 URL 列表
+
+let code = null // 番号
+let searchModeSelects = [] // 搜索模式选择按钮
+let searchCodeBtns = [] // 搜索番号按钮
+let searchIdolBtns = [] // 搜索女优按钮
 
 export async function main(config) {
   // 1. 初始化配置
@@ -39,13 +50,21 @@ export async function main(config) {
   await fetchFavoriteIdols() // 请求 Fav 列表数据
 
   // 4. 书签功能
-  createBookmarkBtn() // 创建书签按钮
+  bookmarkBtns = await createPageBtns({ type: 'bookmark', text: '添加书签', textActive: '书签已添加' }) // 创建书签按钮
   bookmarkBtnClick() // 书签按钮点击事件
 
   // 5. Ban/Fav 功能
   await createBanFavForm() // 创建 Ban/Unban 和 Fav/Unfav 表单
   await initIdolStatus() // 初始化女优状态
   updateIdolStatus() // 更新女优状态
+
+  // 6. 搜索番号/女优
+  code = matchContent('code')
+  searchModeSelects = await createPageBtns({ type: 'search-mode', text: '搜索模式' }) // 创建搜索模式选择按钮
+  searchCodeBtns = await createPageBtns({ type: 'search-code', text: `搜索`, values: [code] }) // 创建搜索番号按钮
+  searchIdolBtns = await createPageBtns({ type: 'search-idols', text: '搜索', values: idolNames }) // 创建搜索女优按钮
+  switchSearchMode() // 切换搜索模式
+  searchBtnsClick() // 点击执行搜索番号/女优
 }
 
 // #region CSS 样式 -------------------------------------------------------
@@ -53,9 +72,9 @@ export async function main(config) {
 /** [功能] 设置 CSS 样式 */
 function setStyle() {
   GM_addStyle(`
-    .bookmark-btn { display: inline-block; padding: 7px 10px; background: #fff; border: 1px solid #ccc; cursor: pointer; }
-    .bookmark-btn:hover { background: #f0f0f0; }
-    .bookmark-btn.active { background: blue; color: #fff; }
+    .page-btn { display: inline-block; padding: 7px 10px; margin-right: 5px; background: #fff; border: 1px solid #ccc; cursor: pointer; }
+    .page-btn:hover { background: #f0f0f0; }
+    .page-btn.active { background: blue; color: #fff; }
 
     /** Ban 和 Fav 表单 */
     .ban-fav-container { width: 300px; position: fixed; top: 20px; right: 20px; padding: 10px; box-sizing: border-box; background-color: #fff; }
@@ -85,31 +104,12 @@ async function fetchFavoriteIdols() {
 
 // #region 书签功能 -------------------------------------------------------
 
-/** [功能] 创建书签按钮 */
-function createBookmarkBtn() {
-  // 1. 创建书签按钮元素
-  bookmarkBtn = createElement({
-    type: 'button',
-    text: '添加书签',
-    cNames: ['bookmark-btn']
-  })
-
-  // 2. 根据 bookmarks 列表设置按钮状态
-  if (bookmarks.includes(id)) {
-    bookmarkBtn.classList.add('active')
-    bookmarkBtn.innerHTML = '书签已添加'
-  }
-
-  // 3. 将按钮添加到页面
-  paginationEl?.append(bookmarkBtn)
-}
-
 /** [功能] 书签按钮点击事件 */
 function bookmarkBtnClick() {
   // 1. 安全检查
-  if (!bookmarkBtn) throw new Error('书签按钮未找到')
+  if (!bookmarkBtns[0]) throw new Error('书签按钮未找到')
 
-  bookmarkBtn.addEventListener('click', (e) => {
+  bookmarkBtns[0].addEventListener('click', (e) => {
     // 2. 阻止默认行为
     e.preventDefault()
 
@@ -172,17 +172,7 @@ async function createBanFavForm() {
 /** [功能] 初始化女优状态 */
 async function initIdolStatus() {
   // 1. 获取页面中所有的女优名字元素
-  const postMsgEl = document.querySelector('#postlist div[id^="post_"] td[id^="postmessage_"]')
-  if (!postMsgEl) return
-  const match = postMsgEl.textContent.match(/(?:【出演女优】|出演者)[:：][ \t]*([^\n\r]*)/)
-  // const match = postMsgEl.textContent.match(/出演者：(?:&nbsp;|\s)*(.*?)/)
-  console.log(postMsgEl.textContent)
-  if (match && match[1]) {
-    idolNames = match[1]
-      .split(/&nbsp;|\s/)
-      .map((name) => name.trim())
-      .filter((name) => name)
-  }
+  idolNames = matchContent('idol')
 
   // 2. 自动填充 Ban/Fav 输入框
   if (idolNames.length > 0) {
@@ -247,11 +237,154 @@ function updateIdolStatus() {
 
 // #endregion
 
+// #region 搜索功能 -------------------------------------------------------
+
+/** [功能] 切换搜索模式 */
+function switchSearchMode() {
+  searchModeSelects[0].addEventListener('change', function () {
+    const mode = this.value
+
+    switch (mode) {
+      case 'sehuatang':
+        searchBaseUrls = [SEHUATANG_SEARCH_BASE_URL]
+        break
+      case 'javbus':
+        searchBaseUrls = [JAVBUS_SEARCH_BASE_URL]
+        break
+      case 'both':
+      default:
+        searchBaseUrls = [SEHUATANG_SEARCH_BASE_URL, JAVBUS_SEARCH_BASE_URL]
+    }
+  })
+}
+
+/** [功能] 点击执行搜索番号/女优 */
+function searchBtnsClick() {
+  ;[...searchCodeBtns, ...searchIdolBtns].forEach((btn) => {
+    btn.addEventListener('click', function () {
+      const isIdolSearch = searchIdolBtns.includes(this)
+      const keyword = this.textContent.replace('搜索', '').trim()
+
+      // 执行搜索，批量打开搜索引擎 URL
+      searchBaseUrls.forEach((baseUrl) => {
+        let searchUrl = ''
+        if (isIdolSearch && (baseUrl.toLowerCase().includes('busjav') || baseUrl.toLowerCase().includes('javbus'))) {
+          searchUrl = baseUrl + 'search/' + encodeURIComponent(keyword)
+        } else {
+          searchUrl = baseUrl + encodeURIComponent(keyword)
+        }
+
+        window.open(searchUrl, '_blank')
+      })
+    })
+  })
+}
+
+// #endregion
+
 // #region 工具方法 -------------------------------------------------------
 
-/** 获取帖子ID */
+/** [工具方法] 获取帖子ID */
 function getId() {
   return Number(url.pathname.startsWith('/forum.php') ? url.searches.tid : url.pathname.split('-')[1])
+}
+
+/**
+ * [工具方法] 匹配指定内容
+ * target: 可选值：idol/code/magnet/torrent
+ */
+function matchContent(target) {
+  let matched = null
+  let match = null
+
+  // 1. 获取帖子内容元素
+  const postMsgEl = document.querySelector('#postlist div[id^="post_"] td[id^="postmessage_"]')
+  const titleEl = document.querySelector('#thread_subject')
+  console.log(titleEl)
+  if (!postMsgEl) return
+
+  // 2. 根据目标类型匹配内容
+  switch (target) {
+    case 'idol':
+      match = postMsgEl.textContent.match(/(?:【出演女优】|出演者)[:：][ \t]*([^\n\r]*)/)
+      // console.log(postMsgEl.textContent)
+      if (match && match[1]) {
+        matched = match[1]
+          .split(/&nbsp;|\s/)
+          .map((name) => name.trim())
+          .filter((name) => name)
+      }
+      break
+    case 'code':
+      match = titleEl.textContent.match(/^(Tokyo Hot\s+n\d+|[a-zA-Z0-9]+-[a-zA-Z0-9-]+(?<!-))/)
+      console.log(match)
+      if (match && match[1]) {
+        matched = match[1].trim().toUpperCase()
+      }
+      break
+    case 'magnet':
+      break
+    case 'torrent':
+      break
+    default:
+  }
+
+  // 3. 返回匹配结果
+  return matched
+}
+
+/** [工具方法] 创建分页行按钮 */
+async function createPageBtns(options = {}) {
+  // 1. 获取变量
+  let { type, text, values = [], textActive = text } = options
+  const btns = []
+
+  // 2. 创建书签按钮元素
+  let i = 0
+  do {
+    let textOrigin = text
+    let btn = null
+
+    if (values.length) {
+      textOrigin = textOrigin + ' ' + values[i]
+    }
+
+    if (type === 'search-mode') {
+      // 3. 特殊处理 search-mode 情况
+      btn = createElement({
+        type: 'select',
+        name: 'mode',
+        cNames: [`${type}-select`, 'page-btn']
+      })
+      btn.innerHTML = `
+        <option value="both" selected>同时搜索</option>
+        <option value="sehuatang">色花堂</option>
+        <option value="javbus">JavBus</option>  
+      `
+    } else {
+      btn = createElement({
+        type: type === 'button',
+        text: textOrigin,
+        cNames: [`${type}-btn`, 'page-btn']
+      })
+    }
+
+    // 3. 特殊处理 bookmark 情况
+    if (type === 'bookmark') {
+      // 根据 bookmarks 列表设置按钮状态
+      if (bookmarks.includes(id)) {
+        btn.classList.add('active')
+        btn.innerHTML = textActive
+      }
+    }
+
+    // 4. 将按钮添加到页面，同时返回按钮元素
+    paginationEl?.append(btn)
+    btns.push(btn)
+    i++
+  } while (i < values.length)
+
+  return btns
 }
 
 // #endregion
